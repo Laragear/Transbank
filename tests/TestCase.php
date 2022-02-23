91b20feab4ea3238f8c3df90705c4ce2d2f0b042
Italo <DarkGhostHunter@Gmail.com>
2022-02-23 14:13:17 -0300

    Fixes package registration in test.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,14 +3,14 @@
 namespace Tests;
 
 use Laragear\Transbank\Facades\Webpay;
-use Laragear\Transbank\TranspayServiceProvider;
+use Laragear\Transbank\TransbankServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     protected function getPackageProviders($app): array
     {
-        return [TranspayServiceProvider::class];
+        return [TransbankServiceProvider::class];
     }
 
     protected function getPackageAliases($app): array
