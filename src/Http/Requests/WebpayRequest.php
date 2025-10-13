5b892beb983ac79af64069e1124eb058a39665d7
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2025-10-13 19:50:10 -0300

    [3.x] Adds opt-in validation for invalid Transbank Requests to app.

--- src/Http/Requests/WebpayRequest.php
+++ src/Http/Requests/WebpayRequest.php
@@ -2,15 +2,25 @@
 
 namespace Laragear\Transbank\Http\Requests;
 
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 use Laragear\Transbank\Services\Transactions\Transaction;
 use Laragear\Transbank\Services\Webpay;
+use function is_bool;
 use function is_callable;
+use function value;
 
 class WebpayRequest extends FormRequest
 {
+    /**
+     * If the request should be validated, and where to redirect when is invalid.
+     *
+     * @var (\Closure():string)|bool
+     */
+    public static Closure|bool $validate = false;
+
     /**
      * The received transaction.
      */
@@ -21,7 +31,35 @@ class WebpayRequest extends FormRequest
      */
     public function validateResolved(): void
     {
-        // Do not validate.
+        static::$validate && parent::validateResolved();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, string|array>
+     */
+    public function rules(): array
+    {
+        return [
+            'TBK_TOKEN' => 'sometimes|required_with:TBK_ORDEN_COMPRA|size:64',
+            'TBK_ORDEN_COMPRA' => 'sometimes|required_with:TBK_TOKEN',
+            'token_ws' => 'sometimes|required_without:TBK_TOKEN|size:64',
+        ];
+    }
+
+    /**
+     * Get the URL to redirect to on a validation error.
+     *
+     * @return string
+     */
+    protected function getRedirectUrl(): string
+    {
+        if (is_callable(static::$validate)) {
+            return $this->container->call(static::$validate);
+        }
+
+        return parent::getRedirectUrl();
     }
 
     /**
@@ -108,7 +146,7 @@ class WebpayRequest extends FormRequest
         // If the request has the `TBK_TOKEN` then the transaction is an error. It's left
         // to the developer if he wants to retrieve the transaction (but shouldn't). If
         // the request doesn't have the `token_ws`, then the user aborted beforehand.
-        return $this->has('TBK_TOKEN') || ! $this->has('token_ws');
+        return $this->has('TBK_TOKEN') || !$this->has('token_ws');
     }
 
     /**
@@ -116,7 +154,7 @@ class WebpayRequest extends FormRequest
      */
     public function isNotError(): bool
     {
-        return ! $this->isError();
+        return !$this->isError();
     }
 
     /**
